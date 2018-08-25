cd20aab32ddcc302e4a210d90c49cfcac8c100f9
Elias Daler <eliasdaler@yandex.ru>
2018-08-25 23:08:32 +0300

    Start working on compat api tests for Window
    Tests added for:
        * Clipboard
        * Context
        * ContextSettings

--- test_projects/api_usage_window.cpp
+++ test_projects/api_usage_window.cpp
@@ -18,3 +18,43 @@
 #include <SFML/Window/Window.hpp>
 #include <SFML/Window/WindowHandle.hpp>
 #include <SFML/Window/WindowStyle.hpp>
+
+// Clipboard.hpp
+void test_clipboard()
+{
+    sf::String str = sf::Clipboard::getString();
+    sf::Clipboard::setString(str);
+}
+
+// Context.hpp
+void test_context()
+{
+    sf::Context context;
+    context.setActive(true);
+    const sf::ContextSettings& settings = context.getSettings();
+    bool isAvailable = sf::Context::isExtensionAvailable("test");
+    sf::GlFunctionPointer funcPtr = sf::Context::getFunction("someFunc");
+    const sf::Context* contextPtr = sf::Context::getActiveContext();
+    sf::Uint64 id = sf::Context::getActiveContextId();
+
+    sf::Context context2(settings, 20u, 20u);
+}
+
+// ContextSettings.hpp
+void test_context_settings()
+{
+    std::initializer_list<sf::ContextSettings::Attribute> var1 = {
+        sf::ContextSettings::Default,
+        sf::ContextSettings::Core,
+        sf::ContextSettings::Debug
+    };
+
+    sf::ContextSettings settings(0u, 0u, 0u, 1u, 1u, sf::ContextSettings::Default, false);
+    settings.depthBits = 1u;
+    settings.stencilBits = 1u;
+    settings.antialiasingLevel = 1u;
+    settings.majorVersion = 1u;
+    settings.minorVersion = 0u;
+    settings.attributeFlags = sf::Uint32(0);
+    settings.sRgbCapable = false;
+}
