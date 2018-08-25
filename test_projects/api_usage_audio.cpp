6035a5a1ab3378a707cd365e76446b5827adbf30
Jonny Paton <jonathan.r.paton@googlemail.com>
2018-08-25 11:07:02 +0100

    Add output sound file, sound and sound buffer API tests

--- test_projects/api_usage_audio.cpp
+++ test_projects/api_usage_audio.cpp
@@ -18,14 +18,17 @@
 #include <SFML/Audio/SoundSource.hpp>
 #include <SFML/Audio/SoundStream.hpp>
 
+// Some globals for testing with multiple apis
+sf::FileInputStream inputStreamFile;
+sf::MemoryInputStream inputStreamMem;
+char data[1] = {0};
+sf::Int16 samples[1] = {0};
+
 void test_input_sound_file()
 {
     sf::InputSoundFile file;
     bool ret = file.openFromFile("/beep/boop/");
-    char data[1] = {0};
     ret = file.openFromMemory(data, std::size_t(1));
-    sf::FileInputStream inputStreamFile;
-    sf::MemoryInputStream inputStreamMem;
     ret = file.openFromStream(inputStreamFile);
     ret = file.openFromStream(inputStreamMem);
     sf::Uint64 sampleCount = file.getSampleCount();
@@ -59,13 +62,55 @@ void test_music()
 {
     sf::Music music;
     bool ret = music.openFromFile("/beep/boop/");
-    char data[1] = {0};
     ret = music.openFromMemory(data, std::size_t(1));
-    sf::FileInputStream inputStreamFile;
-    sf::MemoryInputStream inputStreamMem;
     ret = music.openFromStream(inputStreamFile);
     ret = music.openFromStream(inputStreamMem);
     sf::Time duration = music.getDuration();
     sf::Music::TimeSpan loop = music.getLoopPoints();
     music.setLoopPoints(loop);
+}
+
+void test_output_sound_file()
+{
+    sf::OutputSoundFile file;
+    bool ret = file.openFromFile("/beep/boop/", 12u, 12u);
+    file.write(samples, sf::Uint64(1));
+}
+
+void test_sound()
+{
+    sf::Sound sound;
+    sf::SoundBuffer buf;
+    sf::Sound sound2(buf);
+    sf::Sound sound3(sound2);
+    sound.play();
+    sound.pause();
+    sound.stop();
+    sound.setBuffer(buf);
+    sound.setLoop(true);
+    sound.setPlayingOffset(sf::seconds(1.f));
+    const sf::SoundBuffer* pBuf = sound.getBuffer();
+    bool loop = sound.getLoop();
+    sf::Time offset = sound.getPlayingOffset();
+    sf::Sound::Status status = sound.getStatus();
+    sound = sound2;
+    sound.resetBuffer(); // comment says this is for internal use but is in public API?
+}
+
+void test_sound_buffer()
+{
+    sf::SoundBuffer buffer;
+    sf::SoundBuffer buffer2(buffer);
+    bool ret = buffer.loadFromFile("beep/boop");
+    ret = buffer.loadFromMemory(data, std::size_t(1));
+    ret = buffer.loadFromStream(inputStreamFile);
+    ret = buffer.loadFromStream(inputStreamMem);
+    ret = buffer.loadFromSamples(samples, sf::Uint64(12), 12u, 12u);
+    buffer.saveToFile("beep/boop");
+    const sf::Int16* s = buffer.getSamples();
+    sf::Uint64 count = buffer.getSampleCount();
+    unsigned int rate = buffer.getSampleRate();
+    unsigned int channels = buffer.getChannelCount();
+    sf::Time duration = buffer.getDuration();
+    buffer = buffer2;
 }
