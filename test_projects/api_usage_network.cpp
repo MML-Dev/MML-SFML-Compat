9e7d880ccb603084a3355f888e1ca4b7b707d889
Ceylo <lucas.soltic@orange.fr>
2018-08-25 22:45:32 +0200

    Finish Network compat tests

--- test_projects/api_usage_network.cpp
+++ test_projects/api_usage_network.cpp
@@ -177,3 +177,120 @@ void test_ipaddress_hpp()
     std::cout << r;
     std::cin >> r;
 }
+
+template <typename T>
+void test_pkt_stream()
+{
+    T v = {};
+    sf::Packet pkt;
+    pkt >> v;
+    pkt << v;
+}
+
+void test_packet_hpp()
+{
+    sf::Packet pkt;
+    pkt.append((const void*)nullptr, std::size_t());
+    pkt.clear();
+    const void* data = pkt.getData();
+    std::size_t sz = pkt.getDataSize();
+    bool end = pkt.endOfPacket();
+    bool valid = pkt;
+    
+
+    
+    pkt >> valid;
+    test_pkt_stream<bool>();
+    test_pkt_stream<sf::Int8>();
+    test_pkt_stream<sf::Uint8>();
+    test_pkt_stream<sf::Int16>();
+    test_pkt_stream<sf::Uint16>();
+    test_pkt_stream<sf::Int32>();
+    test_pkt_stream<sf::Uint32>();
+    test_pkt_stream<sf::Int64>();
+    test_pkt_stream<sf::Uint64>();
+    test_pkt_stream<float>();
+    test_pkt_stream<double>();
+    test_pkt_stream<char*>();
+    test_pkt_stream<std::string>();
+    test_pkt_stream<wchar_t*>();
+    test_pkt_stream<std::wstring>();
+    test_pkt_stream<sf::String>();
+}
+
+void test_socket_hpp()
+{
+    sf::Socket::Status st;
+    st = sf::Socket::Done;
+    st = sf::Socket::NotReady;
+    st = sf::Socket::Partial;
+    st = sf::Socket::Disconnected;
+    st = sf::Socket::Error;
+    
+    int v = sf::Socket::AnyPort;
+    
+    sf::TcpSocket skt;
+    skt.setBlocking(true);
+    bool b = skt.isBlocking();
+}
+
+void test_socketselector_hpp()
+{
+    sf::SocketSelector sel;
+    sf::SocketSelector selcopy(sel);
+    selcopy = sel;
+    
+    sf::TcpSocket skt;
+    sel.add(skt);
+    sel.remove(skt);
+    sel.clear();
+    bool b = sel.wait(sf::Time::Zero);
+    b = sel.isReady(skt);
+}
+
+void test_tcplistener_hpp()
+{
+    sf::TcpListener lst;
+    unsigned short lp = lst.getLocalPort();
+    sf::Socket::Status st = lst.listen((unsigned short)0, sf::IpAddress::LocalHost);
+    lst.close();
+    sf::TcpSocket skt;
+    st = lst.accept(skt);
+}
+
+void test_tcpsocket_hpp()
+{
+    sf::TcpSocket skt;
+    unsigned short lp = skt.getLocalPort();
+    sf::IpAddress ip = skt.getRemoteAddress();
+    unsigned short rp = skt.getRemotePort();
+    sf::Socket::Status st = skt.connect(ip, rp, sf::Time::Zero);
+    skt.disconnect();
+    st = skt.send((const void*)nullptr, std::size_t());
+    std::size_t out;
+    st = skt.send((const void*)nullptr, std::size_t(), out);
+    st = skt.receive((void *)nullptr, std::size_t(), out);
+    
+    sf::Packet pkt;
+    st = skt.send(pkt);
+    st = skt.receive(pkt);
+}
+
+void test_udpsocket_hpp()
+{
+    int v = sf::UdpSocket::MaxDatagramSize;
+    
+    sf::UdpSocket skt;
+    unsigned short lp = skt.getLocalPort();
+    sf::IpAddress ip;
+    sf::Socket::Status st = skt.bind((unsigned short)0, ip);
+    skt.unbind();
+    
+    st = skt.send((const void *)nullptr, std::size_t(), ip, lp);
+    std::size_t out;
+    st = skt.receive((void*)nullptr, std::size_t(), out, ip, lp);
+    sf::Packet pkt;
+    st = skt.send(pkt, ip, lp);
+    st = skt.receive(pkt, ip, lp);
+    
+}
