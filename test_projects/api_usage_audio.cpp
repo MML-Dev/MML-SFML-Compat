08f043fbf9b2c5a59751fafca2e5034450956f84
Jonny Paton <jonathan.r.paton@googlemail.com>
2018-08-25 10:06:12 +0100

    Add InputSoundFile API

--- test_projects/api_usage_audio.cpp
+++ test_projects/api_usage_audio.cpp
@@ -17,3 +17,25 @@
 #include <SFML/Audio/SoundRecorder.hpp>
 #include <SFML/Audio/SoundSource.hpp>
 #include <SFML/Audio/SoundStream.hpp>
+
+void test_input_sound_file()
+{
+    sf::InputSoundFile file;
+    bool ret = file.openFromFile("/beep/boop/");
+    char data[1] = {0};
+    ret = file.openFromMemory(data, std::size_t(1));
+    sf::FileInputStream inputStreamFile;
+    sf::MemoryInputStream inputStreamMem;
+    ret = file.openFromStream(inputStreamFile);
+    ret = file.openFromStream(inputStreamMem);
+    sf::Uint64 sampleCount = file.getSampleCount();
+    unsigned int channelCount = file.getChannelCount();
+    unsigned int sampleRate = file.getSampleRate();
+    sf::Time duration = file.getDuration();
+    sf::Time offset = file.getTimeOffset();
+    sf::Uint64 sampleOffset = file.getSampleOffset();
+    file.seek(sf::Uint64(12));
+    file.seek(sf::seconds(12));
+    sf::Int16 samples[1] = {0};
+    sf::Uint64 read = file.read(samples, sf::Uint64(1));
+}
